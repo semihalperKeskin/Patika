a465843fe545b64342a4d99848de053b70022548
Semih Alper Keskin <84216690+semihalperKeskin@users.noreply.github.com>
2022-03-09 01:02:53 +0300

    Add files via upload

---
+++ C# 101 - 7/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Lüften sayı giriniz ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= a; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+
+                }
+
+
+            }
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Yeni Örnek");
+
+            int tek = 0;
+            int çift = 0;
+            for (int i = 0; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    tek += i;
+                }
+
+                else
+                {
+                    çift += i;
+                }
+
+            }
+            Console.WriteLine("Tek sayılar toplamı :" + tek);
+            Console.WriteLine("Çift sayılar toplamı :" + çift);
+
+            Console.WriteLine("Yeni Örnek");
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 5)
+                    break;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Yeni Örnek");
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 5)
+                    continue;
+                Console.WriteLine(i);
+            }
