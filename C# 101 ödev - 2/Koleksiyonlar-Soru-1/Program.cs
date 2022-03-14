d7d8e418e0ac8186fe35485acdf2a68d13e4ea5c
Semih Alper Keskin <84216690+semihalperKeskin@users.noreply.github.com>
2022-03-14 04:15:43 +0300

    Add files via upload

---
+++ C# 101 ödev - 2/Koleksiyonlar-Soru-1/Program.cs
@@ -0,0 +1,108 @@
+﻿
+//Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanara yazınız.)
+// Negatif ve numeric olmayan girişleri engelleyin.
+// Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
+// Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın.
+
+using System.Collections.Generic;
+using System.Collections;
+
+int n = 5;
+ArrayList liste = new ArrayList();
+ArrayList asal = new ArrayList();
+ArrayList noAsal = new ArrayList();
+int sayiGiris=0;
+
+for(int i = 0; i < n ; i++)
+{
+    Console.WriteLine("{0}.Sayınızı giriniz : ", (i+1));
+    try
+    {
+        
+        sayiGiris = int.Parse(Console.ReadLine());
+        while(sayiGiris < 0)
+        {
+            Console.WriteLine("{0}.Sayınız Negatif olamaz. Tekrar Giriniz : ", (i+1));
+            sayiGiris = int.Parse(Console.ReadLine());
+        }
+        liste.Add(sayiGiris);
+    }
+    catch
+    {
+        Console.WriteLine("Hatalı giriş lütfen sayı giriniz : ");
+         Console.WriteLine("{0}.Sayıyı Tekrar Giriniz : ", (i+1));
+        sayiGiris = int.Parse(Console.ReadLine());
+        liste.Add(sayiGiris);
+    }
+}
+
+
+
+
+    foreach(int item in liste)
+    {
+        if(item == 0 || item == 1)
+            noAsal.Add(item);
+
+        else if(item == 2)
+            asal.Add(item);
+
+        else if(item % 2 == 0)
+            noAsal.Add(item);
+
+        else
+            asal.Add(item);
+    }
+Hesaplamalar hesap = new Hesaplamalar();
+
+Console.WriteLine("Asal sayıların büyükten küçüğe sıralanışı : ");
+asal.Sort();
+asal.Reverse();
+
+foreach(int item in asal)
+     Console.WriteLine(asal + " ");
+
+Console.WriteLine("Şeklinde sıralanmıştır.");
+Console.WriteLine("{0} Tane asal sayı vardır.", asal.Count);
+
+
+Console.WriteLine("Asal olmayan sayıların büyükten küçüğe sıralanışı : ");
+noAsal.Sort();
+noAsal.Reverse();
+
+foreach(int item in noAsal)
+     Console.WriteLine(noAsal + " ");
+
+Console.WriteLine("Şeklinde sıralanmıştır.");
+Console.WriteLine("{0} Tane asal olmayan sayı vardır.", noAsal.Count);
+
+Console.WriteLine("Asal sayıların toplamı ve ortapalaması  : ");
+hesap.Ortalama(asal);
+
+Console.WriteLine("Asal olmayan sayıların toplamı ve ortapalaması  : ");
+hesap.Ortalama(noAsal);
+
+
+class Hesaplamalar
+{
+    public ArrayList Asal_Islem(ArrayList arr)
+    {
+        arr.Sort();
+        arr.Reverse();
+        
+        foreach(var item in arr)
+            Console.WriteLine(arr + " ");
+        Console.WriteLine("Şeklinde sıralanmıştır.");
+        return arr;
+    }
+    public async void Ortalama(ArrayList arr)
+    {
+        var sum = 0;
+        var avg = 0;
+        foreach(int item in arr)
+            sum += item;
+        
+        Console.WriteLine("Toplam : "+ sum);
+        Console.WriteLine("Ortalama : "+ (sum/arr.Count));
+    }
+}
---
+++ C# 101 ödev - 2/Koleksiyonlar-Soru-2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Klavyeden girilen 20 adet sayının en büyük 3 tanesi ve en küçük 3 tanesi bulan, her iki grubun kendi içerisinde ortalamalarını alan ve bu ortalamaları ve ortalama toplamlarını console'a yazdıran programı yazınız. (Array sınıfını kullanarak yazınız.
+
+int n = 20;
+int[] sayilar= new int[n];
+int[] büyükSayi= new int[3];
+int[] kücükSayi = new int[3];
+int büyükSayiOrt;
+int kücükSayiOrt;
+
+for(int i = 0; i < n; i++)
+{
+    Console.WriteLine((i+1) +". Sayıyı Giriniz : ");
+    sayilar[i] = int.Parse(Console.ReadLine());
+}
+Array.Sort(sayilar);
+for(int i = 0; i < 3; i++)
+{
+    kücükSayi[i] = sayilar[i];
+}
+Array.Reverse(sayilar);
+for(int i = 0; i < 3; i++)
+{
+    büyükSayi[i] = sayilar[i];
+}
+
+Islemler islem = new Islemler();
+Console.WriteLine("En küçük 3 sayı : ");
+islem.ArrayListe(kücükSayi);
+
+Console.WriteLine("En büyük 3 sayı : ");
+islem.ArrayListe(büyükSayi);
+büyükSayiOrt = islem.Array_Islem(büyükSayi);
+kücükSayiOrt = islem.Array_Islem(kücükSayi);
+
+Console.WriteLine("Küçük sayıların ortalaması : "+ kücükSayiOrt);
+Console.WriteLine("Büyük sayıların ortalaması : "+ büyükSayiOrt);
+
+Console.WriteLine("Ortalamalar Toplamı : "+ (kücükSayiOrt + büyükSayiOrt));
+
+class Islemler
+{
+    public int Array_Islem(int[] arr)
+    {
+        var sum = arr.Sum();
+        var ortalamalar = sum / arr.Length;
+        return ortalamalar;
+    }
+    public void ArrayListe(int[] arr)
+    {
+        foreach(var item in arr)
+            Console.WriteLine(item + " ");
+    }
+}
